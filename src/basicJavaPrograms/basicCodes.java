4bc98fbf3d5331a4af39eea7ac581157be26fd51
prkrishnakumarqa <prkrishnakumar@qainfotech.com>
2018-07-06 16:55:04 +0530

    Pattern

--- src/basicJavaPrograms/basicCodes.java
+++ src/basicJavaPrograms/basicCodes.java
@@ -3,15 +3,40 @@ package basicJavaPrograms;
 public class basicCodes {
 
 	public static void main(String[] args) {
-		int i,j,temp;
-		
-		for(i=5;i>=0;i--)
+		int i,j,z,temp=1;
+		int x=1;		
+		for(i=1;i<=8;i++)
 		{
-			
-			
-				System.out.println("*");
-			
+			for(j=1;j<=i;j++)
+			{
+				if(i<=4)
+				{
+				if(j==i)
+				{
+					System.out.print(j);
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				}
+				else
+				{
+					if(j==i-x)
+					{
+						System.out.print(j);
+						x=x+2;
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+					
+				}
+			}
+			System.out.print("\n");
+		}
 
 	}
 
-}}
+}
